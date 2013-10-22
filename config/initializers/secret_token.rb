c55fb174882cc1a38781355ad8f6b3fe5d105256
kevolution2013 <kevinbet6@gmail.com>
2013-10-22 14:51:30 +0300

    chagua

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chagua::Application.config.secret_key_base = 'dba503b6c05d303c4109025dd15f5755ee284f6872bd1d1608bdbc6b2568e6f7d5c7e7e1ae21a8e280a2206b0c3b5b6448b193622b22020f187d6932f00f5e5c'
